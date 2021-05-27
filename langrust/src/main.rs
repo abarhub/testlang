c0c9e2902b3206a2b858abcc26ffb07670b06117
abarhub <tulipebleu1@laposte.net>
2021-05-27 19:11:45 +0200

    ajout de la gestion des parametres dans rust

--- langrust/src/main.rs
+++ langrust/src/main.rs
@@ -1,6 +1,6 @@
+use std::env;
 
-
-fn randomNumber(max: u32,mut last:&mut u64) -> u32
+fn random_number(max: u32, last:&mut u64) -> u32
 {
     let tmp2:u64=*last;
     let tmp01=(25214903917u128 * tmp2 as u128) as u64;
@@ -12,11 +12,11 @@ fn randomNumber(max: u32,mut last:&mut u64) -> u32
     return (tmp % (max as u64)) as u32;
 }
 
-fn createArray(len:u32, maxValue:u32,mut last:&mut u64)-> Vec<u32>
+fn create_array(len:u32, max_value:u32,mut last:&mut u64)-> Vec<u32>
 {
     let mut result1:Vec<u32> = Vec::new();
-    for i in 1..(len + 1) {
-        result1.push(randomNumber(maxValue,&mut last));
+    for _i in 1..(len + 1) {
+        result1.push(random_number(max_value,&mut last));
     }
     return result1;
 }
@@ -24,21 +24,46 @@ fn createArray(len:u32, maxValue:u32,mut last:&mut u64)-> Vec<u32>
 
 fn main() {
 
-    let mut tabSize:u32 = 10000000;
-    let maxValue:u32 = 5000000;
-    let noOutput = false;
-    let debug = false;
+    let mut tab_size:u32 = 10000000;
+    let max_value:u32 = 5000000;
+    let mut no_output = false;
+    let mut debug = false;
     let mut last:u64=0;
 
-    tabSize=10;
+    let args: Vec<String> = env::args().collect();
 
-    let mut a = [1,3,2];
-    a.sort();
-    println!("{:?}", a);
+    for x in args.iter() {
+        if x=="--nooutput"{
+            no_output=true;
+        } else if x.starts_with("--nbop=") {
+            let s=&x[7..];
+            let n: u32 = s
+                .trim()
+                .parse()
+                .expect("Wanted a number");
+            if n>0 {
+                tab_size=n;
+            }
+        } else if x=="--debug" {
+            debug = true;
+        }
+    }
 
-    let mut vec=createArray(tabSize,maxValue,&mut last);
+    let mut vec=create_array(tab_size,max_value,&mut last);
 
     vec.sort();
 
-    println!("Collected (0..10) into: {:?}", vec);
+    if debug {
+        let mut i:u32=0;
+        for x in args.iter()         {
+            println!("   argv[{}] : '{}'\n", i,x);
+            i=i+1;
+        }
+        println!("lang=rust;tabSize={};maxValue={};noOutput={};debug={}",
+               tab_size, max_value, no_output, debug);
+    }
+
+    if !no_output{
+        println!("Collected (0..10) into: {:?}",  &vec[..10]);
+    }
 }
